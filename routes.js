43fe8d7941c567135967919981852194903f711c
Your Name <you@example.com>
2018-05-14 12:38:54 +0530

    fcm

--- routes.js
+++ routes.js
@@ -21,14 +21,41 @@ router.get('/', function(req, res) {
 router.post('/botHandler',/*Authentication.SetRealm('botHandler'), Authentication.BasicAuthentication, */function(req, res){
 	
 	console.log('req received');
-	res.end();
+	var resp = {
+    "conversationToken": "",
+    "expectUserResponse": true,
+    "expectedInputs": [
+        {
+            "inputPrompt": {
+                "richInitialPrompt": {
+                    "items": [
+                        {
+                            "simpleResponse": {
+                                "textToSpeech": "Howdy! I can tell you fun facts about almost any number, like 42. What do you have in mind?",
+                                "displayText": "Howdy! I can tell you fun facts about almost any number. What do you have in mind?"
+                            }
+                        }
+                    ],
+                    "suggestions": []
+                }
+            },
+            "possibleIntents": [
+                {
+                    "intent": "actions.intent.TEXT"
+                }
+            ]
+        }
+    ]
+}
+	res.json(resp).end();
 });
 router.post('/botHandler2',/*Authentication.SetRealm('botHandler'), Authentication.BasicAuthentication, */function(req, res){
 	
 	console.log('req received to botHandler2');
 	var resp = {						
 			
-				"speech": "its me"				
+				"speech": "its me",
+				"text": "its me"
 			}
 	res.json(resp).end();
 });

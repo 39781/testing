f3e0723cbafbce7fd98653b041bc46ac68606c01
Your Name <you@example.com>
2018-05-23 17:19:53 +0530

    first commit

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ var router			= express.Router();
 var request			= require('request');	
 var fs 				= require("fs");	
 var request			= require('request');
+const {URLSearchParams} = require('url')
 var path			= require("path");	
 var config			= require('./config');	
 const uuidv1 		= require('uuid/v1');
@@ -11,6 +12,14 @@ const authToken = '574bce6f3f25f3f744b1cf08e39ca8c3';
 const client = require('twilio')(accountSid, authToken);
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
 const twimlResponse = new VoiceResponse();
+var botRep={
+	"hello":"Hi. I am calling to book an appointment for a client, Mr. John who wants to insure the assets of his candy manufacturing business. I am looking for your time , sometime tomorrow at 3 pm",
+	"I have another meeting at 3 pm which will get over by 4; so the earliest I can call Mr. John is around 4.15 pm":"Do you have any availability between 2 to 4 pm tomorrow?",
+	"Sure. I can call Mr. John at 2.30 pm tomorrow":"2.30 pm is fine",
+	"Can you provide the coordinates of Mr. John":"His mobile number is xxx xxx xxxx and his email id is xxxx@gmail.com",
+	"Thanks. I have made a note of it and I will call Mr. John at 2.30 pm tomorrow":"Sure. I will do that. Thanks",
+	"Great. Have a good day. Bye":"bye"
+}
 //var Authentication = require('./utilities/Authentication');
 
 
@@ -19,13 +28,18 @@ router.get('/', function(req, res) {
 	console.log('hari');
   res.send("welcome to macy");
 });
+
 router.get('/reply',function(req, res){
 	console.log(req.url);
+	var q = url.parse(req.url, true).query;
+	console.log(req.url);
+	var txt = q.SpeechResult.replace(/+/,' ');
+	
 	res.end();
 });
-router.post('/answer',function(req, res){
-	/*console.log(req,'req received');
-	twimlResponse.say('Thanks for contacting our sales department. Our ' +
+router.get('/answer',function(req, res){
+	console.log(req,'req received');
+	/*twimlResponse.say('Thanks for contacting our sales department. Our ' +
 	  'next available representative will take your call. ',
 	  { voice: 'alice' });
 
@@ -40,7 +54,7 @@ router.post('/answer',function(req, res){
 	  action:'/reply',
 	  method:'GET'
 	});
-	gather.say('Hi. I am calling to book an appointment for a client, Mr. John who wants to insure the assets of his candy manufacturing business. I am looking for your time , sometime tomorrow at 3 pm');
+	gather.say(botRep["hello"]);
 	
 	res.writeHead(200, { 'Content-Type': 'text/xml' });
     res.end(response.toString());
@@ -51,7 +65,8 @@ router.get('/call',function(req, res){
 	  .create({
 		url: 'https://fast-reef-26757.herokuapp.com/answer',
 		to: '+918500050085',
-		from: '+1 913-705-4764'		
+		from: '+1 913-705-4764',
+		method:"GET"	
 	  })
 	  .then(call => {
 		  	console.log(call.sid);					  

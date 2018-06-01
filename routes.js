2626405269d1cb82b9cb180111829db52efa7e82
Your Name <you@example.com>
2018-06-01 10:49:14 +0530

    first commit

--- routes.js
+++ routes.js
@@ -91,7 +91,8 @@ router.get('/call',function(req, res){
 	client.calls
 	  .create({
 		url: 'https://fast-reef-26757.herokuapp.com/answer?SpeechResult=Hello&cid='+req.query.cid,
-		to: '+919597439539',
+		//to: '+919597439539',
+		to: '+918500050085',
 		from: '+1 913-705-4764',
 		method:"GET"	
 	  })
